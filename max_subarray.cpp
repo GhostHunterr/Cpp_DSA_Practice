d0d83c7fe8d05cd8005f695019f7d21b5c1163cf
Farhan <farhansarang7@gmail.com>
2024-03-21 14:54:24 -0400

    Solved: Maximum Subarray.

---
+++ max_subarray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+    int maxSum = nums[0];
+    int currSum = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        currSum += nums[i];
+        if (maxSum < currSum)
+        {
+            maxSum = currSum;
+        }
+        currSum = (currSum < 0) ? 0 : currSum;
+    }
+
+    return maxSum;
+}
+
+int main()
+{
+    return 0;
+}
